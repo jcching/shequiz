0af544159c15c32fad673477974b495188643e5e
ceapas <jcching135246@gmail.com>
2016-04-11 21:52:28 +0800

    splash code update

--- js/webappInstall.js
+++ js/webappInstall.js
@@ -2,7 +2,7 @@
 //detects if the webpage is installed on mobile, if not, load splash.html to force user install
 
 
-if (window.navigator.standalone == true) {
+if (window.location.protocol == 'app:') {
  //leave things alone
 
 }else {
