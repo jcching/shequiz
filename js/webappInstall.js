d5845bec86b612eb7a0b4f1112463d9b169ae2e2
ceapas <jcching135246@gmail.com>
2016-04-11 21:42:24 +0800

    returned user control for iOS apps

--- js/webappInstall.js
+++ js/webappInstall.js
@@ -1,3 +1,9 @@
 //webappInstall.js
 //detects if the webpage is installed on mobile, if not, load splash.html to force user install
-$( "body" ).load( "splash.html" );
+
+
+if (window.navigator.standalone == true) {
+ //leave things alone
+} else {
+ $( "body" ).load( "splash.html" );
+}
