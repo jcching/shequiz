9ca5285033666254f0a6b4d48931857560d18bc5
ceapas <jcching135246@gmail.com>
2016-04-11 21:55:35 +0800

    log debug

--- js/webappInstall.js
+++ js/webappInstall.js
@@ -1,10 +1,5 @@
 //webappInstall.js
 //detects if the webpage is installed on mobile, if not, load splash.html to force user install
 
-
-if (window.location.protocol == 'app:') {
- //leave things alone
-
-}else {
- $( "body" ).load( "splash.html" );
-}
+console.log("test page loaded, status is:");
+console.log(window.location.protocol);
