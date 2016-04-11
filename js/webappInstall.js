42f59ab3a68ea367f498e0a5ac65d3c4090df691
ceapas <jcching135246@gmail.com>
2016-04-11 21:59:02 +0800

    4

--- js/webappInstall.js
+++ js/webappInstall.js
@@ -2,7 +2,9 @@
 //detects if the webpage is installed on mobile, if not, load splash.html to force user install
 
 console.log("test page loaded, status is:");
-console.log(window.location.protocol);
+
+var currentProtocol=window.location.protocol;
+console.log(currentProtocol);
 
 // if (window.location.protocol == 'app:') {
 //  //leave things alone
@@ -11,7 +13,7 @@ console.log(window.location.protocol);
 //  $( "body" ).load( "splash.html" );
 // }
 
-if (window.location.protocol == "app:"){
+if (currentProtocol == "app:"){
 	console.log("say ahh");
 }else{
 	console.log("say no");
