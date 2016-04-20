f03fa7c5a56f4c12ac99897d74e012f68ed70895
ceapas <jcching135246@gmail.com>
2016-04-21 00:55:09 +0800

    let

--- js/logic.js
+++ js/logic.js
@@ -430,13 +430,13 @@ function shuffle(array) {
     // While there are elements in the array
     while (counter > 0) {
         // Pick a random index
-        let index = Math.floor(Math.random() * counter);
+        var index = Math.floor(Math.random() * counter);
 
         // Decrease counter by 1
         counter--;
 
         // And swap the last element with it
-        let temp = array[counter];
+        var temp = array[counter];
         array[counter] = array[index];
         array[index] = temp;
     }
